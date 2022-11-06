2866290f634527c7586f5aecdb4b6839c77e85b6
kanaydo <batara.girsang@outlook.com>
2022-11-06 15:12:23 +0700

    removing unused code

--- src/utils/validators/unique.validator.ts
+++ src/utils/validators/unique.validator.ts
@@ -1,27 +1,5 @@
-// import { Injectable } from "@nestjs/common";
-// import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
-
-// @ValidatorConstraint({ name: 'EmailExistsRule', async: true })
-// @Injectable()
-// export class EmailExistsRule implements ValidatorConstraintInterface {
-//   validate(value: any, args?: ValidationArguments | undefined): boolean | Promise<boolean> {
-//     console.log(`==========================================> validating ${value}`);
-//     return false;
-//   }
-
-//   defaultMessage?(validationArguments?: ValidationArguments | undefined): string {
-//     return `email already used!`;
-//   }
-// }
-
-import {
-  registerDecorator,
-  ValidationOptions,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
-  ValidationArguments,
-} from 'class-validator';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
 import { DataSource } from 'typeorm';
 
 @ValidatorConstraint({ name: 'Unique', async: true })
@@ -32,9 +10,7 @@ export class UniqueConstraint implements ValidatorConstraintInterface {
 
   async validate(value: any, args: ValidationArguments): Promise<boolean> {
     const [model, property = 'id', exceptField = null] = args.constraints;
-
     const current = await this.dataSource.getRepository(model).createQueryBuilder("q").where(`q.${property} = :term`, { term: value }).getOne();
-    // console.log('================>', current);
     if (current === null) return true;
 
     return false;
